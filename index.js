9f54af49c355050814a9989c12da9b55cb14f238
Juan Pablo Suarez Ramirez <86664381+juanp-suarezr@users.noreply.github.com>
2022-07-29 22:36:39 -0500

    actualizacion responsive

---
+++ index.js
@@ -0,0 +1,10 @@
+$( document ).ready(function() {
+
+    if (window.matchMedia("(max-width: 400px)").matches) {
+        
+        $('.responsive-media').attr('src', 'libro1/celular.html');
+        } else {
+        // Viewport is greater than 700 pixels wide
+        }
+})
+
